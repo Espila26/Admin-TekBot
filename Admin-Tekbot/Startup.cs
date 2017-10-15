98f924e52d5deb7f560f78039acd0d58e9469f77
User01 <User01@DevelopmentVM>
2017-10-15 20:08:37 +0000

    Add project files.

---
+++ Admin-Tekbot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Admin_Tekbot.Startup))]
+namespace Admin_Tekbot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
