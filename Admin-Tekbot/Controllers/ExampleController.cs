7068c91d19ba893398912b3f98a41cd3ba504ed3
User01 <User01@DevelopmentVM>
2017-10-19 03:55:02 +0000

    Changes to User Modell and the views for this model

---
+++ Admin-Tekbot/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin_Tekbot.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Admin-Tekbot/Controllers/UsersController.cs
+++ Admin-Tekbot/Controllers/UsersController.cs
@@ -61,6 +61,31 @@ namespace Admin_Tekbot.Controllers
             return View(user);
         }
 
+        //// GET: Users/Create
+        //public ActionResult RequestVacations()
+        //{
+        //    ViewBag.TeamId = new SelectList(db.Teams, "TeamId", "TeamName");
+        //    return View();
+        //}
+
+        //// POST: Users/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult RequestVacations([Bind(Include = "mail,UserName,TeamId,Last_Review,Users_Status,Users_positions,Users_vacations")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(user);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.TeamId = new SelectList(db.Teams, "TeamId", "TeamName", user.TeamId);
+        //    return View(user);
+        //}
+
         // GET: Users/Edit/5
         public ActionResult Edit(string id)
         {
